2f5cf9a8102c0cf70e6a9ddfd8aebe81bf7409d6
raphael <rafadastan02@live.com>
2020-05-29 12:58:00 -0300

    finish project the selective process in b2w

--- src/main/java/com/b2w/processoseletivo/springboot/application/PlanetService.java
+++ src/main/java/com/b2w/processoseletivo/springboot/application/PlanetService.java
@@ -1,11 +1,19 @@
 package com.b2w.processoseletivo.springboot.application;
 
 import com.b2w.processoseletivo.springboot.Presentation.Models.Planets;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponents;
+import org.springframework.web.util.UriComponentsBuilder;
+
 import java.util.List;
 import java.util.Arrays;
 
 public class PlanetService {
+
+    @Autowired
+    RestTemplate template = new RestTemplate();
 
     public List<Planets> getAll(){
         String url = "https://swapi.dev/api/planets/";
@@ -14,6 +22,21 @@ public class PlanetService {
         Planets[] planets = restTemplate.getForObject("https://swapi.dev/api/planets/",Planets[].class);
 
         return Arrays.asList(planets);
+    }
+
+    public void consumerApi(){
+        //https://swapi.dev/api/planets/?search=Tatooine
+
+        UriComponents uri = UriComponentsBuilder.newInstance()
+                .scheme("https")
+                .host("swapi.dev")
+                .path("api/planets")
+                .queryParam("search", "Tatooine")
+                .build();
+
+        ResponseEntity<Planets> entity = template.getForEntity(uri.toUriString(), Planets.class);
+
+        entity.getBody().getName();
     }
 
 }
